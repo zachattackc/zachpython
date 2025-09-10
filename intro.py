dd699e68c459f561a0dd374fc63e579670c8bc2a
Ben Collier <benjamin.collier@gmail.com>
2025-09-09 20:25:35 -0700

    done makign the green square move

--- intro.py
+++ intro.py
@@ -5,9 +5,11 @@ pygame.init()
 WIDTH = 800
 HEIGHT = 600
 CELL_SIZE = 20
+direction_x = 1
+direction_y = 1
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Snake Game - Step 1")
+pygame.display.set_caption("Snake Game ")
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -20,17 +22,20 @@ GRAY = (128, 128, 128)
 CYAN = (0, 255, 255)
 PINK = (255, 192, 203)
 
+rect_x = WIDTH // 2 - CELL_SIZE // 2 #middle of the screen
+rect_y = HEIGHT // 2 - CELL_SIZE // 2 #middle of the screen
+
 clock = pygame.time.Clock()
 running = True 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            rect_x += direction_x * CELL_SIZE
+            rect_y += direction_y * CELL_SIZE
         screen.fill(BLACK)
-        rect_x = WIDTH // 2 - CELL_SIZE // 2
-        rect_y = HEIGHT // 2 - CELL_SIZE // 2
         pygame.draw.rect(screen, GREEN, (rect_x, rect_y, CELL_SIZE, CELL_SIZE))
-        pygame.display.flip()
-    clock.tick(60)
+        pygame.display.flip() #update the screen
+    clock.tick(60) #60 fps
 pygame.quit()
 
